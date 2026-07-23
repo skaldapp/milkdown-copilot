aafb9d19bfc220b3d318f79a9df7330a067b015d
Jerry Bruwes <jbruwes@gmail.com>
2026-07-23 16:20:25 +0200

    chore: inject parserCtx and serializerCtx dependencies

--- src/index.ts
+++ src/index.ts
@@ -95,6 +95,8 @@ export const copilotPlugin = [
     ctx.inject(modelSlice);
     ctx.inject(baseURLSlice);
     ctx.inject(endpointSlice);
+    ctx.inject(parserCtx);
+    ctx.inject(serializerCtx);
     return () => undefined;
   },
   $prose((ctx) => {
